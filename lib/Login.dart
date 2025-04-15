26dd9a6575ed5dadbc1a22826fdb5668df47709f
BoraComert <boramcomert@gmail.com>
2025-04-15 10:34:31 +0300

    a new login file

--- lib/Login.dart
+++ lib/Login.dart
@@ -9,10 +9,85 @@ class LoginClass extends StatefulWidget {
 }
 
 class _LoginClassState extends State<LoginClass> {
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+   Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          "SuConnect",
+          style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Welcome Back",
+              style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+            ),
+            Text(
+              "Log in with your Sabanci Email",
+              style: TextStyle(fontSize: 18),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(30.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  labelText: 'email@sabanciuniv.edu',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(left: 30.0, right: 30.0),
+              child: TextField(
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Password',
+                  border: OutlineInputBorder(),
+                ),
+              ),
+            ),
+            SizedBox(height: 30),
+            TextButton(
+              onPressed: () {
+                // Add login logic here
+              },
+              style: TextButton.styleFrom(
+                minimumSize: Size(345, 60),
+                backgroundColor: Colors.black,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+              ),
+              child: Text("Log In", style: TextStyle(color: Colors.white)),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 18.0),
+              child: Text(
+                "Forgot your password?",
+                style: TextStyle(fontSize: 15),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                // Navigate to password reset or support
+              },
+              style: TextButton.styleFrom(
+                minimumSize: Size(30, 20),
+                backgroundColor: Colors.white10,
+                shape: const RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(8)),
+                ),
+              ),
+              child: Text(
+                "Reset Password",
+                style: TextStyle(color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
-
 }
